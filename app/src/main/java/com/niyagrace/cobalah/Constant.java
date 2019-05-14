fe59816744d29b9d8ff535b6e1048a797afa43ff
Nia Gracelita <drstar.grace@gmail.com>
2019-05-14 19:01:09 +0700

    Initial commit

---
+++ app/src/main/java/com/niyagrace/cobalah/Constant.java
@@ -0,0 +1,15 @@
+package com.niyagrace.cobalah;
+
+/**
+ * Created by fariz ramadhan.
+ * website : www.farizdotid.com
+ * github : https://github.com/farizdotid
+ * linkedin : https://www.linkedin.com/in/farizramadhan/
+ */
+
+
+public class Constant {
+    public static final String KEY_ID_MATKUL = "keyIdMatkul";
+    public static final String KEY_NAMA_DOSEN = "keyNamaDosen";
+    public static final String KEY_MATKUL = "keyMatkul";
+}
